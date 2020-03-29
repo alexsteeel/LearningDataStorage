ed60db74c763372fb554477788ffcc17183baf18
alexsteeel <rf-89@list.ru>
2020-03-29 18:38:39 +0300

    Add service model folder and class Language.

---
+++ LearningDataStorage/Models/Service/Language.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearningDataStorage
+{
+    /// <summary>
+    /// Язык.
+    /// </summary>
+    public class Language
+    {
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Наименование.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Код языка по ISO 639.
+        /// </summary>
+        [MaxLength(3)]
+        public string Code { get; set; }
+    }
+}
