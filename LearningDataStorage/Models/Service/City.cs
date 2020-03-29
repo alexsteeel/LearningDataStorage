fb8c3a55b66f63109234d2e15dff1d87210e2d62
alexsteeel <rf-89@list.ru>
2020-03-29 22:48:39 +0300

    Add service classes City and Country.

---
+++ LearningDataStorage/Models/Service/City.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LearningDataStorage
+{
+    /// <summary>
+    /// Город.
+    /// </summary>
+    [Table("Cities", Schema = "srv")]
+    public class City
+    {
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Id страны.
+        /// </summary>
+        public int CountryId { get; set; }
+
+        /// <summary>
+        /// Страна.
+        /// </summary>
+        public virtual Country Country { get; set; }
+    }
+}
---
+++ LearningDataStorage/Models/Service/Country.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LearningDataStorage
+{
+    /// <summary>
+    /// Страна.
+    /// </summary>
+    [Table("Countries", Schema = "srv")]
+    public class Country
+    {
+        public Country()
+        {
+            Cities = new List<City>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Города.
+        /// </summary>
+        public ICollection<City> Cities { get; set; }
+    }
+}
