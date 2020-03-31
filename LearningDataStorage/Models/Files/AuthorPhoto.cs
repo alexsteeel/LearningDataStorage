ec02de04275c11f58bb84757bff56fed6c932f3f
alexsteeel <rf-89@list.ru>
2020-03-31 22:01:17 +0300

    Change file info tables AuthorPhoto and BookCover.

--- LearningDataStorage/Models/Files/AuthorPhoto.cs
+++ LearningDataStorage/Models/Files/AuthorPhoto.cs
@@ -10,56 +10,12 @@ namespace LearningDataStorage
     [Table("AuthorPhoto", Schema = "file")]
     public class AuthorPhoto : IPhoto
     {
+        /// <summary>
+        /// Guid файла из файловой таблицы.
+        /// </summary>
         [Key]
-        [Column("stream_id")]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public Guid StreamId { get; set; }
-
-        [Column("file_stream")]
-        public byte[] FileStream { get; set; }
-
-        [Column("name")]
-        [StringLength(255)]
-        public string Name { get; set; }
-
-        [Column("file_type")]
-        [StringLength(255)]
-        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-        public string FileType { get; set; }
-
-        [Column("cached_file_size")]
-        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-        public long? CachedFileSize { get; set; }
-
-        [Column("creation_time")]
-        public DateTimeOffset CreationTime { get; set; }
-
-        [Column("last_write_time")]
-        public DateTimeOffset LastWriteTime { get; set; }
-
-        [Column("last_access_time")]
-        public DateTimeOffset? LastAccessTime { get; set; }
-
-        [Column("is_directory")]
-        public bool IsDirectory { get; set; }
-
-        [Column("is_offline")]
-        public bool IsOffline { get; set; }
-
-        [Column("is_hidden")]
-        public bool IsHidden { get; set; }
-
-        [Column("is_readonly")]
-        public bool IsReadOnly { get; set; }
-
-        [Column("is_archive")]
-        public bool IsArchive { get; set; }
-
-        [Column("is_system")]
-        public bool IsSystem { get; set; }
-
-        [Column("is_temporary")]
-        public bool IsTemporary { get; set; }
 
         /// <summary>
         /// Id автора.
