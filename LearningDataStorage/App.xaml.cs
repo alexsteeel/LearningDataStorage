a5a0fe1a7a354a497988be01df9866c471127e1d
alexsteeel <rf-89@list.ru>
2020-04-04 19:27:39 +0300

    Add localizations.

--- LearningDataStorage/App.xaml.cs
+++ LearningDataStorage/App.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
+using System.Globalization;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace LearningDataStorage
@@ -13,5 +11,68 @@ namespace LearningDataStorage
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            InitializeComponent();
+            LanguageChanged += App_LanguageChanged;
+
+            Languages.Clear();
+            Languages.Add(new CultureInfo("en-US"));
+            Languages.Add(new CultureInfo("ru-RU"));
+
+            Language = LearningDataStorage.Properties.Settings.Default.DefaultLanguage;
+        }
+
+        public static List<CultureInfo> Languages { get; } = new List<CultureInfo>();
+
+        public static event EventHandler LanguageChanged;
+
+        public static CultureInfo Language
+        {
+            get
+            {
+                return System.Threading.Thread.CurrentThread.CurrentUICulture;
+            }
+            set
+            {
+                if (value == null) throw new ArgumentNullException("value");
+                if (value == System.Threading.Thread.CurrentThread.CurrentUICulture) return;
+
+                System.Threading.Thread.CurrentThread.CurrentUICulture = value;
+
+                ResourceDictionary dict = new ResourceDictionary();
+                switch (value.Name)
+                {
+                    case "ru-RU":
+                        dict.Source = new Uri(string.Format($"Resources/lang.{value.Name}.xaml"), UriKind.Relative);
+                        break;
+                    default:
+                        dict.Source = new Uri("Resources/lang.xaml", UriKind.Relative);
+                        break;
+                }
+
+                ResourceDictionary oldDict = (from d in Current.Resources.MergedDictionaries
+                                              where d.Source != null && d.Source.OriginalString.StartsWith("Resources/lang.")
+                                              select d).First();
+                if (oldDict != null)
+                {
+                    int ind = Current.Resources.MergedDictionaries.IndexOf(oldDict);
+                    Current.Resources.MergedDictionaries.Remove(oldDict);
+                    Current.Resources.MergedDictionaries.Insert(ind, dict);
+                }
+                else
+                {
+                    Current.Resources.MergedDictionaries.Add(dict);
+                }
+
+                LanguageChanged(Current, new EventArgs());
+            }
+        }
+
+        private void App_LanguageChanged(object sender, EventArgs e)
+        {
+            LearningDataStorage.Properties.Settings.Default.DefaultLanguage = Language;
+            LearningDataStorage.Properties.Settings.Default.Save();
+        }
     }
 }
