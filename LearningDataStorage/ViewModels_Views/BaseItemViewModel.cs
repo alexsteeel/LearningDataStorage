bd02d77a01101a1e8e4dacdf8274b2145717d114
alexsteeel <suchkov.as.1989@gmail.com>
2020-05-24 17:49:38 +0300

    Replace common code for entity view models in BaseEntityViewModel.

---
+++ LearningDataStorage/ViewModels_Views/BaseItemViewModel.cs
@@ -0,0 +1,55 @@
+﻿using FluentValidation;
+using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace LearningDataStorage
+{
+    public class BaseEntityViewModel : INotifyPropertyChanged, IDataErrorInfo
+
+    {
+        private readonly IValidator _validator;
+
+        public BaseEntityViewModel(IValidator validator)
+        {
+            _validator = validator;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string this[string columnName]
+        {
+            get
+            {
+                var error = _validator.Validate(this).Errors
+                    .FirstOrDefault(x => x.PropertyName == columnName);
+
+                if (error != null)
+                {
+                    return _validator != null ? error.ErrorMessage : "";
+                }
+                else
+                {
+                    return "";
+                }
+            }
+        }
+
+        public string Error
+        {
+            get
+            {
+                if (_validator != null)
+                {
+                    var results = _validator.Validate(this);
+                    if (results != null && results.Errors.Any())
+                    {
+                        var errors = string.Join(Environment.NewLine, results.Errors.Select(x => x.ErrorMessage).ToArray());
+                        return errors;
+                    }
+                }
+                return string.Empty;
+            }
+        }
+    }
+}
--- LearningDataStorage/ViewModels_Views/City/CityViewModel.cs
+++ LearningDataStorage/ViewModels_Views/City/CityViewModel.cs
@@ -1,20 +1,12 @@
 ﻿using LearningDataStorage.Core.Models;
-using System;
-using System.ComponentModel;
-using System.Linq;
 
 namespace LearningDataStorage
 {
-    public class CityViewModel : INotifyPropertyChanged, IDataErrorInfo
+    public class CityViewModel : BaseEntityViewModel
     {
-        private readonly CityValidator _cityValidator;
-
-        public CityViewModel()
+        public CityViewModel() : base(new CityValidator())
         {
-            _cityValidator = new CityValidator();
         }
-
-        public event PropertyChangedEventHandler PropertyChanged;
 
         public int Id { get; set; }
 
@@ -22,40 +14,5 @@ namespace LearningDataStorage
 
         public int CountryId { get; set; }
         public Country Country { get; set; }
-
-        public string this[string columnName]
-        {
-            get
-            {
-                var error = _cityValidator.Validate(this).Errors
-                    .FirstOrDefault(x => x.PropertyName == columnName);
-
-                if (error != null)
-                {
-                    return _cityValidator != null ? error.ErrorMessage : "";
-                }
-                else
-                {
-                    return "";
-                }                
-            }
-        }
-
-        public string Error
-        {
-            get
-            {
-                if (_cityValidator != null)
-                {
-                    var results = _cityValidator.Validate(this);
-                    if (results != null && results.Errors.Any())
-                    {
-                        var errors = string.Join(Environment.NewLine, results.Errors.Select(x => x.ErrorMessage).ToArray());
-                        return errors;
-                    }
-                }
-                return string.Empty;
-            }
-        }
     }
 }
