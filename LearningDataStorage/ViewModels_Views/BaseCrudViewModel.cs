cc98cd457a9536f3688ca13ef7a26eaaf30cd146
alexsteeel <suchkov.as.1989@gmail.com>
2020-05-24 21:29:39 +0300

    Add BaseCrudViewModel.

---
+++ LearningDataStorage/ViewModels_Views/BaseCrudViewModel.cs
@@ -0,0 +1,118 @@
+﻿using AutoMapper;
+using log4net;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LearningDataStorage
+{
+    public abstract class BaseCrudViewModel : BindableBase, IInitialized
+    {
+        protected readonly ILog _log;
+        protected readonly ResourceDictionary _localization;
+        protected readonly IDialog _dialog;
+        protected readonly ISingletonContainer _mainContainer;
+        protected readonly IMapper _mapper;
+
+        protected BaseCrudViewModel(ISingletonContainer mainContainer)
+        {
+            _log = mainContainer.Log;
+            _localization = mainContainer.Localization;
+            _dialog = mainContainer.Dialog;
+            _mainContainer = mainContainer;
+            _mapper = mainContainer.Mapper;
+
+            CreateCommand = new DelegateCommand(Create);
+            UpdateCommand = new DelegateCommand(Update);
+            DeleteCommand = new DelegateCommand(Delete);
+            SaveCommand = new DelegateCommand(Save);
+        }
+
+        #region Properties
+
+        public bool IsLoading { get; set; }
+
+        public bool IsDialogOpen { get; set; }
+
+        #endregion Properties
+
+        #region Commands
+
+        public DelegateCommand CreateCommand { get; set; }
+
+        public DelegateCommand UpdateCommand { get; set; }
+
+        public DelegateCommand DeleteCommand { get; set; }
+
+        public DelegateCommand SaveCommand { get; set; }
+
+        #endregion Commands
+
+        #region Methods
+
+        public async void Init()
+        {
+            IsLoading = true;
+            try
+            {
+                await InitAsync();
+            }
+            catch (Exception ex)
+            {
+                var errorText = $"{_localization["m_Er_InitError"]}{_localization["m_Er_DetailedError"]}";
+                _log.Error(errorText, ex);
+                _dialog.Error($"{errorText} {ex.Message}");
+            }
+            finally
+            {
+                IsLoading = false;
+            }
+        }        
+
+        private async void Save()
+        {
+            try
+            {
+                await SaveAsync();
+            }
+            catch (Exception ex)
+            {
+                var errorText = $"{_localization["m_Er_SaveError"]}{_localization["m_Er_DetailedError"]}";
+                _log.Error(errorText, ex);
+                _dialog.Error($"{errorText} {ex.Message}");
+            }
+            finally
+            {
+                IsDialogOpen = false;
+            }
+        }
+
+        public async void Delete()
+        {
+            try
+            {
+                await DeleteAsync();
+            }
+            catch (Exception ex)
+            {
+                var errorText = $"{_localization["m_Er_DeleteError"]}{_localization["m_Er_DetailedError"]}";
+                _log.Error(errorText, ex);
+                _dialog.Error($"{errorText} {ex.Message}");
+            }
+        }
+
+        public abstract Task InitAsync();
+
+        public abstract Task SaveAsync();
+
+        public abstract void Create();
+
+        public abstract void Update();
+
+        public abstract Task DeleteAsync();
+
+        #endregion Methods
+    }
+}
--- LearningDataStorage/ViewModels_Views/City/CitiesListViewModel.cs
+++ LearningDataStorage/ViewModels_Views/City/CitiesListViewModel.cs
@@ -1,20 +1,17 @@
-﻿using AutoMapper;
-using LearningDataStorage.Core.Models;
+﻿using LearningDataStorage.Core.Models;
 using LearningDataStorage.Core.Services;
 using MaterialDesignThemes.Wpf;
-using Prism.Commands;
-using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace LearningDataStorage
 {
-    public class CitiesListViewModel : BaseViewModel, IInitialized
+    public class CitiesListViewModel : BaseCrudViewModel
     {
         private readonly IService<City> _cityService;
         private readonly IService<Country> _countryService;
-        private readonly IMapper _mapper;
 
         public CitiesListViewModel(ISingletonContainer mainContainer, ICommonServicesContainer servicesContainer)
             : base(mainContainer)
@@ -22,15 +19,8 @@ namespace LearningDataStorage
             _cityService = servicesContainer.CityService;
             _countryService = servicesContainer.CountryService;
 
-            _mapper = mainContainer.Mapper;
-
             Cities = new ObservableCollection<CityViewModel>();
             Countries = new ObservableCollection<Country>();
-
-            SaveCityCommand = new DelegateCommand(SaveCity);
-            AddCityCommand = new DelegateCommand(AddCity);
-            ChangeCityCommand = new DelegateCommand(ChangeCity);
-            DeleteCityCommand = new DelegateCommand(DeleteCity);
         }
 
         #region Properties
@@ -41,51 +31,21 @@ namespace LearningDataStorage
 
         public ObservableCollection<Country> Countries { get; set; }
 
-        public bool IsLoading { get; set; }
-
-        public bool IsDialogOpen { get; set; }
-
-        #endregion Properties
-
-        #region Commands
-
-        public DelegateCommand SaveCityCommand { get; set; }
-
-        public DelegateCommand AddCityCommand { get; set; }
-
-        public DelegateCommand ChangeCityCommand { get; set; }
-
-        public DelegateCommand DeleteCityCommand { get; set; }
-
-        #endregion Commands
+        #endregion Properties        
 
         #region Methods
 
-        public async void Init()
+        public override async Task InitAsync()
         {
-            IsLoading = true;
-            try
-            {
-                var cities = await _cityService.GetAll();
-                var countries = await _countryService.GetAll();
+            var cities = await _cityService.GetAll();
+            var countries = await _countryService.GetAll();
 
-                var cityViewModels = _mapper.Map<IEnumerable<City>, IEnumerable<CityViewModel>>(cities);
-                Cities = new ObservableCollection<CityViewModel>(cityViewModels);
-                Countries = new ObservableCollection<Country>(countries);
-            }
-            catch (Exception ex)
-            {
-                var errorText = $"{_localization["m_Er_InitCitiesError"]}{_localization["m_Er_DetailedError"]}";
-                _log.Error(errorText, ex);
-                _dialog.Error($"{errorText} {ex.Message}");
-            }
-            finally
-            {
-                IsLoading = false;
-            }
+            var cityViewModels = _mapper.Map<IEnumerable<City>, IEnumerable<CityViewModel>>(cities);
+            Cities = new ObservableCollection<CityViewModel>(cityViewModels);
+            Countries = new ObservableCollection<Country>(countries);
         }
 
-        private async void AddCity()
+        public override async void Create()
         {
             SelectedCity = new CityViewModel
             {
@@ -95,57 +55,34 @@ namespace LearningDataStorage
             await DialogHost.Show(SelectedCity, "CityDialog");
         }
 
-        private async void ChangeCity()
+        public override async void Update()
         {
             await DialogHost.Show(SelectedCity, "CityDialog");
         }
 
-        private async void SaveCity()
+        public override async Task SaveAsync()
         {
-            try
+            if (SelectedCity.Id != 0)
             {
-                if (SelectedCity.Id != 0)
-                {
-                    var oldCity = await _cityService.GetById(SelectedCity.Id);
-                    var city = _mapper.Map<CityViewModel, City>(SelectedCity);
-                    await _cityService.Update(oldCity, city);
-                }
-                else
-                {
-                    var city = _mapper.Map<CityViewModel, City>(SelectedCity);
-                    await _cityService.Create(city);
-                    Cities.Add(SelectedCity);
-                }
+                var oldCity = await _cityService.GetById(SelectedCity.Id);
+                var city = _mapper.Map<CityViewModel, City>(SelectedCity);
+                await _cityService.Update(oldCity, city);
             }
-            catch (Exception ex)
+            else
             {
-                var errorText = $"{_localization["m_Er_SaveCitiesError"]}{_localization["m_Er_DetailedError"]}";
-                _log.Error(errorText, ex);
-                _dialog.Error($"{errorText} {ex.Message}");
-            }
-            finally
-            {
-                IsDialogOpen = false;
+                var city = _mapper.Map<CityViewModel, City>(SelectedCity);
+                await _cityService.Create(city);
+                Cities.Add(SelectedCity);
             }
         }
 
-        private async void DeleteCity()
+        public override async Task DeleteAsync()
         {
-            try
-            {
-                var city = await _cityService.GetById(SelectedCity.Id);
-                await _cityService.Delete(city);
-                Cities.Remove(SelectedCity);
-            }
-            catch (Exception ex)
-            {
-                var errorText = $"{_localization["m_Er_DeleteCitiesError"]}{_localization["m_Er_DetailedError"]}";
-                _log.Error(errorText, ex);
-                _dialog.Error($"{errorText} {ex.Message}");
-            }
+            var city = await _cityService.GetById(SelectedCity.Id);
+            await _cityService.Delete(city);
+            Cities.Remove(SelectedCity);
         }
 
         #endregion Methods
-
     }
 }
